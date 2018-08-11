69ddf465d6421257a7d0608860d7a3f16e36d106
Digory <digory1@googlemail.com>
2018-08-11 13:55:32 +0100

    Add getter methods to shop

--- src/main/shop/Shop.java
+++ src/main/shop/Shop.java
@@ -1,4 +1,70 @@
 package main.shop;
 
+import main.accessories.Accessory;
+import main.instrument.Instrument;
+import main.instrument.InstrumentType;
+import main.interfaces.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private ArrayList<ISell> stock;
+
+    public Shop(){
+        stock = new ArrayList<>();
+    }
+
+    public void addToStock(ISell item){
+        stock.add(item);
+    }
+
+    public boolean removeItem(ISell item){
+        return stock.remove(item);
+    }
+
+    public ArrayList getAllStock(){
+        return stock;
+    }
+
+    public double calculatePotentialProfit(){
+        double total = 0;
+        for(ISell item : stock){
+            total += item.calculateMarkup();
+        }
+        return total;
+    }
+
+    public boolean removeAllInThisCategory(InstrumentType category){
+         boolean instrumentsRemoved = removeInstrumentsInThisCategory(category);
+         boolean accessoriesRemoved = removeAccessoriesInThisCategory(category);
+         return instrumentsRemoved || accessoriesRemoved;
+    }
+
+    public boolean removeInstrumentsInThisCategory(InstrumentType category){
+        boolean anyRemoved = false;
+        for(ISell item : stock){
+            if(item instanceof Instrument){
+                if(((Instrument) item).getCategory() == category){
+                    stock.remove(item);
+                    anyRemoved = true;
+                }
+            }
+        }
+        return anyRemoved;
+    }
+
+    public boolean removeAccessoriesInThisCategory(InstrumentType category){
+        boolean anyRemoved = false;
+        for(ISell item : stock){
+            if(item instanceof Accessory){
+                if(((Accessory) item).getCategory() == category){
+                    stock.remove(item);
+                    anyRemoved = true;
+                }
+            }
+        }
+        return anyRemoved;
+    }
+
 }
--- src/test/ShopTest.java
+++ src/test/ShopTest.java
@@ -1,4 +1,55 @@
 package test;
 
+import main.accessories.GuitarPlectrum;
+import main.accessories.TriangleBaton;
+import main.accessories.TrumpetSheetMusic;
+import main.instrument.ColourType;
+import main.instrument.Guitar;
+import main.instrument.Triangle;
+import main.instrument.Trumpet;
+import main.shop.Shop;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    private Shop shop;
+    private Guitar guitar;
+    private Triangle triangle;
+    private Trumpet trumpet;
+    private GuitarPlectrum plectrum;
+    private TriangleBaton baton;
+    private TrumpetSheetMusic sheetMusic;
+
+    @Before
+    public void before(){
+        shop = new Shop();
+        guitar = new Guitar(ColourType.GREEN, 12, 13.0);
+        triangle = new Triangle(ColourType.BLUE, 4.0);
+        trumpet = new Trumpet(ColourType.WHITE, 4, 22.0);
+        plectrum = new GuitarPlectrum(0.5);
+        baton = new TriangleBaton(1.5);
+        sheetMusic = new TrumpetSheetMusic(8.0);
+    }
+
+    @Test
+    public void addToStock_instruments(){
+        shop.addToStock(guitar);
+        shop.addToStock(triangle);
+        shop.addToStock(trumpet);
+        assertEquals(3, shop.getAllStock().size());
+    }
+
+    @Test
+    public void addToStock_instruments_and_accessories(){
+        shop.addToStock(guitar);
+        shop.addToStock(triangle);
+        shop.addToStock(trumpet);
+        shop.addToStock(plectrum);
+        shop.addToStock(baton);
+        shop.addToStock(sheetMusic);
+        assertEquals(6, shop.getAllStock().size());
+    }
 }
