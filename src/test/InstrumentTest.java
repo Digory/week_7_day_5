67fee0d44bdc05a8b5c7a6bf775a46561fdc36b1
Digory <digory1@googlemail.com>
2018-08-11 12:24:52 +0100

    Instrument getter tests pass

--- src/test/InstrumentTest.java
+++ src/test/InstrumentTest.java
@@ -1,4 +1,41 @@
 package test;
 
+import main.instrument.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class InstrumentTest {
+
+    private Guitar guitar;
+    private Triangle triangle;
+    private Trumpet trumpet;
+
+    @Before
+    public void before(){
+        guitar = new Guitar(ColourType.BLACK, 6);
+        triangle = new Triangle(ColourType.PINK);
+        trumpet = new Trumpet(ColourType.BLUE, 4);
+    }
+
+    @Test
+    public void getCategoryReturnsCorrect(){
+        assertEquals(InstrumentType.STRING, guitar.getCategory());
+    }
+
+    @Test
+    public void getNumberOfStringsReturnsCorrect(){
+        assertEquals(6, guitar.getNumberOfStrings());
+    }
+
+    @Test
+    public void getNumberOfValvesReturnsCorrect(){
+        assertEquals(4, trumpet.getNumberOfValves());
+    }
+
+    @Test
+    public void playReturnsCorrect(){
+        assertEquals("ding...", triangle.play());
+    }
 }
